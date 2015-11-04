dcf547b0ef58c0337cacabe954a1f7eb610d121a
Cairo Stewart <stewartcairo@gmail.com>
2015-11-04 14:06:20 -0800

    done

--- script.js
+++ script.js
@@ -1,30 +1,34 @@
 var body = document.getElementsByTagName('body');
-var body = document.getElementsByTagName('body');
+var html = document.getElementsByTagName('html');
+var masterDiv = document.createElement('div')
+masterDiv.className = 'master';
+var color = 82;
 var myArr = [];
+function colorizer_base(temp){
+	temp.style.backgroundColor = 'rgb(365, ' + color + ', 100' + ')';
+	temp.style.width = '11.1%';
+	temp.style.height = '125px'
+	temp.style.float = 'left';
+}
+
+function colorizer_gradient(temp){
+	temp.style.backgroundColor = 'rgb(180, ' + color + ', 19' + ')';
+	temp.style.width = '11.1%';
+	temp.style.height = '125px';
+	temp.style.float = 'left';
+}
 
 for(var i = 0; i < (9*7); i++){
-	var temp = document.createElement('div')
+	color += 4;
+	var temp = document.createElement('div');
+	if(i % 2 === 0){
+			colorizer_base(temp);
+		} else {
+			colorizer_gradient(temp);
+		}
 	myArr.push(temp);
 }
 
-function Colorizer(arr){
-	var counter = 0;
-	for (var i = 0; i < arr.length; i++) {
-		counter ++;
-		var num = Math.floor(Math.random() * 900000) + 100000;
-		if(counter % 2 === 0){
-			arr[i].style.backgroundColor = '#' + num.toString();
-			arr[i].style.width = '11.1%';
-			arr[i].style.height = '125px'
-			arr[i].style.float = 'left';
-			body[0].appendChild(arr[i]);
-		} else {
-			arr[i].style.backgroundColor = '#' + num.toString();
-			arr[i].style.width = '11.1%';
-			arr[i].style.height = '125px';
-			arr[i].style.float = 'left';
-			body[0].appendChild(arr[i]);
-		}
-	};
-}
-Colorizer(myArr);
+for (var i = 0; i < myArr.length; i++) {
+	body[0].appendChild(myArr[i]);
+};
