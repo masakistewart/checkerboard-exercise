45209da97517d489d8943317b495182b78871340
Cairo Stewart <stewartcairo@gmail.com>
2015-11-04 12:07:28 -0800

    fixed

--- script.js
+++ script.js
@@ -1,22 +1,25 @@
 var body = document.getElementsByTagName('body');
 var html = document.getElementsByTagName('html');
+var masterDiv = document.createElement('div')
+masterDiv.className = 'master';
 var myArr = [];
-
 for(var i = 0; i < (9*7); i++){
 	var temp = document.createElement('div');
-	if(counter % 2 === 0){
+	if(i % 2 === 0){
 			temp.style.backgroundColor = 'red';
 			temp.style.width = '11.1%';
 			temp.style.height = '125px'
 			temp.style.float = 'left';
-			body[0].appendChild(temp);
 		} else {
 			temp.style.backgroundColor = 'blue';
 			temp.style.width = '11.1%';
 			temp.style.height = '125px';
 			temp.style.float = 'left';
-			body[0].appendChild(temp);
 		}
 	myArr.push(temp);
 }
+
+for (var i = 0; i < myArr.length; i++) {
+	body[0].appendChild(myArr[i]);
+};
 
