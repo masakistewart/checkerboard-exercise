796c5c3e1e1c04c6f4282b5e26f0b676f0d82f5d
Cairo Stewart <stewartcairo@gmail.com>
2015-11-04 11:21:08 -0800

    done

--- script.js
+++ script.js
@@ -1,1 +1,30 @@
-// Your JS goes here
+var body = document.getElementsByTagName('body');
+var body = document.getElementsByTagName('body');
+var myArr = [];
+
+for(var i = 0; i < (9*7); i++){
+	var temp = document.createElement('div')
+	myArr.push(temp);
+}
+
+function Colorizer(arr){
+	var counter = 0;
+	for (var i = 0; i < arr.length; i++) {
+		counter ++;
+		var num = Math.floor(Math.random() * 900000) + 100000;
+		if(counter % 2 === 0){
+			arr[i].style.backgroundColor = '#' + num.toString();
+			arr[i].style.width = '11.1%';
+			arr[i].style.height = '125px'
+			arr[i].style.float = 'left';
+			body[0].appendChild(arr[i]);
+		} else {
+			arr[i].style.backgroundColor = '#' + num.toString();
+			arr[i].style.width = '11.1%';
+			arr[i].style.height = '125px';
+			arr[i].style.float = 'left';
+			body[0].appendChild(arr[i]);
+		}
+	};
+}
+Colorizer(myArr);
