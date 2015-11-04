fe0f88b279d22b8987c5cef78f186843d813f174
Cairo Stewart <stewartcairo@gmail.com>
2015-11-04 15:48:53 -0800

    somewhat complete

--- script.js
+++ script.js
@@ -1,30 +1,34 @@
 var body = document.getElementsByTagName('body');
 var html = document.getElementsByTagName('html');
-var masterDiv = document.createElement('div')
-masterDiv.className = 'master';
+var div = document.getElementsByTagName('div');
+var x;
 var color = 82;
 var myArr = [];
 function colorizer_base(temp){
 	temp.style.backgroundColor = 'black';
+}
+
+function setSize(temp){
 	temp.style.width = '11.1%';
 	temp.style.height = '125px'
 	temp.style.float = 'left';
 }
 
 function colorizer_gradient(temp){
-	temp.style.backgroundColor = 'rgb(280, ' + color + ', 19' + ')';
-	temp.style.width = '11.1%';
-	temp.style.height = '125px';
-	temp.style.float = 'left';
+	var rand = function(){ return Math.floor(Math.random() * 900) + 200 };
+	temp.style.backgroundColor = 'rgb(' + rand() + ', ' + color + ', ' + rand() + ')';
 }
+var rand = function(){ return Math.floor(Math.random() * 900) + 100 };
 
 for(var i = 0; i < (9*7); i++){
 	color += 4;
 	var temp = document.createElement('div');
 	if(i % 2 === 0){
 			colorizer_base(temp);
+			setSize(temp)
 		} else {
 			colorizer_gradient(temp);
+			setSize(temp);
 		}
 	myArr.push(temp);
 }
@@ -33,5 +37,14 @@ for (var i = 0; i < myArr.length; i++) {
 	body[0].appendChild(myArr[i]);
 };
 
+window.onload = function() {
+    var currentColor = 'red';
+    setInterval(function() {
+    	currentColor = currentColor === 'red' ? 'green' : 'red';
+        for (var i = 0; i < div.length; i++) {
+        	div[i].style.backgroundColor = 'rgb(' + rand() + ', ' + color + ', ' + rand() + ')';
+        };
+    }, 1000);
+};
 
 
